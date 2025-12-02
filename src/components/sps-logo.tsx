4aec25e439c3cec57bf8c739ab001d05a3a7a943
Simon Peters <simonpetersgh@gmail.com>
2025-12-02 13:01:56 +0000

    make it the image as logo circular, it is original not

--- src/components/sps-logo.tsx
+++ src/components/sps-logo.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import type { ComponentProps } from "react";
+import { cn } from "@/lib/utils";
 
 export const SpsLogo = (props: Partial<ComponentProps<typeof Image>>) => {
-  const { width = 48, height = 48, ...rest } = props;
+  const { width = 48, height = 48, className, ...rest } = props;
   return (
     <Image
       src="/sp-icon.png"
       alt="SPS Studio Logo"
       width={width}
       height={height}
+      className={cn("rounded-full", className)}
       {...rest}
     />
   );
